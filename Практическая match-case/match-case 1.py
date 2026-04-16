57b57fe1ca2e8cb283e06a1a29acf4e29b2b6f22
tavkka <240608kolinko@gmail.com>
2026-04-16 14:01:59 +0300

    Update match-case 1.py

--- Практическая match-case/match-case 1.py
+++ Практическая match-case/match-case 1.py
@@ -3,10 +3,10 @@ match month:
     case 12 | 1 | 2:
         print(f"Это {month} месяц, это зима ❄️")
     case 3 | 4 | 5:
-        print(f"Это {month} месяц, это весна 🌸.")
+        print(f"Это {month} месяц, это весна 🌸")
     case 6 | 7 | 8:
-        print(f"Это {month} месяц, это лето ☀️.")
+        print(f"Это {month} месяц, это лето ☀️")
     case 9 | 10 | 11:
-        print(f"Это {month} месяц, это осень 🍂.")
+        print(f"Это {month} месяц, это осень 🍂")
     case _:
         print("Введено некорректное число")
