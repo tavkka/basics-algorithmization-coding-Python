21169c8fb2e6aab28b984cb00b0115439b231afd
tavkka <240608kolinko@gmail.com>
2026-06-09 15:18:31 +0300

    Update задача 1.py

--- практическая 10/задача 1.py
+++ практическая 10/задача 1.py
@@ -4,8 +4,10 @@ password = 4590
 
 while True:
     ask = int(input("Введите пароль: "))
+    
     if ask == password:
         print("Доступ разрешен")
         break
+        
     if ask != password:
         print("Ошибка. Попробуйте ещё раз")
