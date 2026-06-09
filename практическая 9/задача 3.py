be87dc28c8f8e95f8bc07f61582e245d3effd249
tavkka <240608kolinko@gmail.com>
2026-06-09 15:46:59 +0300

    Add files via upload

---
+++ практическая 9/задача 3.py
@@ -0,0 +1,18 @@
+print("Задача про ведьмака")
+
+cost = int(input("Введите цену ваших услуг: "))
+moneta = 0
+
+while cost >= 25:
+    moneta += 1
+    cost -= 25
+while cost >= 10:
+    moneta += 1
+    cost -= 10
+while cost >= 5:
+    moneta += 1
+    cost -= 5
+while cost >= 1:
+    moneta += 1
+    cost -= 1
+print(f"Минимальное количество трубуемых монет {moneta}")
